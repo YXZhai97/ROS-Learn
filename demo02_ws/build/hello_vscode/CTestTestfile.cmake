ee4f7f1a495f4e1475ffb7e36a23991ccf0e66ca
Yixing <yixing.zhai97@gmail.com>
2021-10-07 22:51:24 +0200

    create catkin workspace demo02

---
+++ demo02_ws/build/hello_vscode/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yixing/Desktop/Github/ROS-Learn/demo02_ws/src/hello_vscode
+# Build directory: /home/yixing/Desktop/Github/ROS-Learn/demo02_ws/build/hello_vscode
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ demo02_ws/src/hello_vscode/src/hello_vscode_c.cpp
@@ -0,0 +1,8 @@
+#include "ros/ros.h"
+int main(int argc, char *argv[])
+{
+    ros::init(argc,argv,"hello_c");
+    ROS_INFO("hello");
+
+    return 0;
+}
