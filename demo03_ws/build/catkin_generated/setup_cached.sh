d7277d5e72245ee7c1d326f04f2c4d7c95753565
“github’s <eason.zhai97@outlook.com>
2021-10-16 14:09:09 +0200

    add self-defined msg

--- demo03_ws/build/catkin_generated/setup_cached.sh
+++ demo03_ws/build/catkin_generated/setup_cached.sh
@@ -11,5 +11,6 @@ export CMAKE_PREFIX_PATH="/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/devel:
 export LD_LIBRARY_PATH="/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/build'
+export PYTHONPATH="/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/yixing/Desktop/Github/ROS-Learn/demo03_ws/src:$ROS_PACKAGE_PATH"
